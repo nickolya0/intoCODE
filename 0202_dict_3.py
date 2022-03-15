8300a13c0bfe6713f949b0d7628beff711ef8b26
nickolya0 <88684302+nickolya0@users.noreply.github.com>
2022-03-15 16:20:52 +0100

    Add files via upload

---
+++ 0202_dict_3.py
@@ -0,0 +1,18 @@
+dict = {                                # initialisen wir ein Dictionary. key-value.
+    'Name':         'Mykola',
+    # 'Nachname':     'Zitser',
+    'Alter':        '36', # -
+    # 'Grosse':       '183',
+    'Wohnort':      [] # -
+
+}
+WohnortList = ['Bismarkstr', 2, 30100]                      # initialisieren wir eine Liste
+WohnortList1 = ['Bahnhof', 6, 30700]
+
+
+def dictManipulieren(dict):               # definieren wir eine Funktion mit übergabene parameter
+
+    dict['Wohnort'] = [WohnortList, WohnortList1]
+
+dictManipulieren(dict)
+print(dict)
